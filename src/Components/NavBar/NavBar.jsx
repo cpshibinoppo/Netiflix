57da03327556d85bb79b821f12a41dd3a6282d99
cpshibinoppo <cpshibinoppo@gmail.com>
2023-07-20 15:22:13 +0530

    first commit

---
+++ src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './NavBar.css';
+
+function NavBar() {
+  return (
+    <div className='container mx-auto'>
+        <img className='w-48' src="/img/logos/netfilixLogo.svg" alt="logo" />
+    </div>
+  )
+}
+
+export default NavBar
